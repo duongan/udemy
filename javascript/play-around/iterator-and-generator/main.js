2f06a766d6a4ff293d9a2e752a8e57c5bc8f2af0
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-18 21:14:18 +0700

    playing around with Iterator and Generator 2

--- javascript/play-around/iterator-and-generator/main.js
+++ javascript/play-around/iterator-and-generator/main.js
@@ -67,10 +67,81 @@ const company = {
 
 // const itNameOnly = company.getEmployeeNameOnly()
 
-for (const obj of company.getEmployeeNameOnly()) {
-	console.log(obj);
+// for (const obj of company.getEmployeeNameOnly()) {
+// 	console.log(obj);
+// }
+
+// for (const obj of company.getFullEmployeeInfo()) {
+// 	console.log(obj);
+// }
+
+class Sequence {
+	constructor(start = 0, end = Infinity, interval = 1) {
+		this.start = start;
+		this.end = end;
+		this.interval = interval;
+	}
+
+	[Symbol.iterator]() {
+		let count = 0;
+		let nextIndex = this.start;
+		return {
+			next: () => {
+				if (nextIndex <= this.end) {
+					let result = { value: nextIndex, done: false };
+					nextIndex += this.interval;
+					count++;
+					return result;
+				}
+				return { value: count, done: true };
+			},
+			return: () => {
+				console.log('cleaning up...');
+				return { value: undefined, done: true };
+			},
+		};
+	}
 }
 
-for (const obj of company.getFullEmployeeInfo()) {
-	console.log(obj);
+const seq = new Sequence(1, 10, 2);
+console.log('seq', seq);
+// for (const num of seq) {
+// 	if (num < 7) {
+// 		console.log('num', num);
+// 		break;
+// 	}
+// }
+
+// console.log(seq.next());
+
+class Sequence2 {
+	constructor(start = 0, end = Infinity, interval = 1) {
+		this.start = start;
+		this.end = end;
+		this.interval = interval;
+	}
+	*[Symbol.iterator]() {
+		let nextIndex = this.start;
+		while (nextIndex <= this.end) {
+			const result = nextIndex;
+			nextIndex += this.interval;
+			yield result;
+		}
+	}
+	*sayHello() {
+		let nextIndex = this.start;
+		while (nextIndex <= this.end) {
+			yield `Hello ${nextIndex}`;
+			nextIndex++;
+		}
+	}
+}
+
+const seq2 = new Sequence2(1, 10, 2);
+console.log('seq2', seq2);
+for (const num of seq2) {
+	console.log('num.2', num);
+}
+for (const obj of seq2.sayHello()) {
+	console.log('obj', obj);
 }
