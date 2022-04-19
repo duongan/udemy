ee5e3f69e13a5240ac4aa8be073ff36f2fd812cf
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-19 23:06:02 +0700

    Section 4: Working with State, Events

--- javascript/code-along-tutor/01-starting-setup/src/App.js
+++ javascript/code-along-tutor/01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
 	const expenses = [
@@ -27,9 +28,14 @@ function App() {
 			date: new Date(2021, 5, 12),
 		},
 	];
+
+	const addExpenseHandler = (expense) => {
+		console.log('App.js', expense);
+	};
+
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense onAddExpense={addExpenseHandler} />
 			<Expenses items={expenses} />
 		</div>
 	);
--- javascript/code-along-tutor/01-starting-setup/src/components/Expenses/Expenses.js
+++ javascript/code-along-tutor/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,11 +1,23 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+	const [filteredYear, setFilteredYear] = useState('2020');
 	const { items = [] } = props;
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
 	return (
 		<Card className="expenses">
+			<ExpensesFilter
+				onFilter={filterChangeHandler}
+				selected={filteredYear}
+			/>
 			{items.map((item) => (
 				<ExpenseItem
 					key={item.id}
