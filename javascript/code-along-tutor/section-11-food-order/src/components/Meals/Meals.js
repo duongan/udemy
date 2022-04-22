ba745afb7c273e99e18f168fcf260cf7de634852
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-22 13:09:20 +0700

    Section 11: Food Order App - Adding Meals components

---
+++ javascript/code-along-tutor/section-11-food-order/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import MealsSummary from './MealsSummary';
+import AvailabelMeals from './AvailableMeals';
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvailabelMeals />
+        </Fragment>
+    );
+};
+
+export default Meals;
