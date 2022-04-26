af89d2ffab14e229c15ea79c0e3a518674e2d7c0
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-26 16:53:06 +0700

    Section 16: Basic Form - self implementing - apply useReducer

--- javascript/code-along-tutor/section-16-working-with-forms/src/hooks/use-form.js
+++ javascript/code-along-tutor/section-16-working-with-forms/src/hooks/use-form.js
@@ -1,27 +1,60 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const inputDefaultState = {
+    value: '',
+    isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+    if (action.type === 'INPUT') {
+        return {
+            value: action.value,
+            isTouched: state.isTouched,
+        };
+    }
+    if (action.type === 'BLUR') {
+        return {
+            isTouched: true,
+            value: state.value,
+        };
+    }
+    if (action.type === 'RESET') {
+        return { ...inputDefaultState };
+    }
+    return inputDefaultState;
+};
 
 const useForm = (validateFn) => {
-    const [valueInput, setValueInput] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+    const [inputState, dispatch] = useReducer(
+        inputStateReducer,
+        inputDefaultState
+    );
+    // const [valueInput, setValueInput] = useState('');
+    // const [isTouched, setIsTouched] = useState(false);
 
-    const isValid = validateFn(valueInput);
-    const hasError = !isValid && isTouched;
+    // const isValid = validateFn(valueInput);
+    // const hasError = !isValid && isTouched;
+    const isValid = validateFn(inputState.value);
+    const hasError = !isValid && inputState.isTouched;
 
     const inputChangeHandler = (event) => {
-        setValueInput(event.target.value);
+        dispatch({ type: 'INPUT', value: event.target.value });
+        // setValueInput(event.target.value);
     };
 
     const inputBlurHandler = (event) => {
-        setIsTouched(true);
+        dispatch({ type: 'BLUR' });
+        // setIsTouched(true);
     };
 
     const reset = () => {
-        setValueInput('');
-        setIsTouched(false);
+        dispatch({ type: 'RESET' });
+        // setValueInput('');
+        // setIsTouched(false);
     };
 
     return {
-        value: valueInput,
+        value: inputState.value,
         isValid,
         hasError,
         inputChangeHandler,
