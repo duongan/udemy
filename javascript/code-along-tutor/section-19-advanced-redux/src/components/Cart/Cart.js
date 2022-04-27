029227608003852daa5036c7fb4711691d1de70f
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-27 21:39:45 +0700

    Section 19: Advanced Redux - Completed practicing

--- javascript/code-along-tutor/section-19-advanced-redux/src/components/Cart/Cart.js
+++ javascript/code-along-tutor/section-19-advanced-redux/src/components/Cart/Cart.js
@@ -1,21 +1,37 @@
+import { useSelector } from 'react-redux';
+
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+    const items = useSelector((state) => state.cart.items);
+
+    let cartContent = <p>No items.</p>;
+
+    if (items.length) {
+        cartContent = (
+            <ul>
+                {items.map((item) => (
+                    <CartItem
+                        key={item.id}
+                        item={{
+                            id: item.id,
+                            title: item.title,
+                            quantity: item.quantity,
+                            total: item.totalPrice,
+                            price: item.price,
+                        }}
+                    />
+                ))}
+            </ul>
+        );
+    }
+
     return (
         <Card className={classes.cart}>
             <h2>Your Shopping Cart</h2>
-            <ul>
-                <CartItem
-                    item={{
-                        title: 'Test Item',
-                        quantity: 3,
-                        total: 18,
-                        price: 6,
-                    }}
-                />
-            </ul>
+            {cartContent}
         </Card>
     );
 };
---
+++ javascript/code-along-tutor/section-19-advanced-redux/src/store/cart-slice.js
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    items: [],
+    totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(
+                (item) => item.id === newItem.id
+            );
+            state.totalQuantity++;
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    title: newItem.title,
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice += newItem.price;
+            }
+        },
+        removeItem(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            state.totalQuantity--;
+            if (existingItem) {
+                if (existingItem.quantity === 1) {
+                    state.items = state.items.filter((item) => item.id !== id);
+                } else {
+                    existingItem.quantity--;
+                    existingItem.totalPrice -= existingItem.price;
+                }
+            }
+        },
+        toggleCart(state) {
+            state.showCart = !state.showCart;
+        },
+    },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
