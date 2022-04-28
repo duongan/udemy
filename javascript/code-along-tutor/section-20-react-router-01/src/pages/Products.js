bd39b195568994509aca4e433efd6af9daabaddc
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-28 15:29:21 +0700

    Section 20: React Router - Project 01 - Playing around with React Router completed

--- javascript/code-along-tutor/section-20-react-router-01/src/pages/Products.js
+++ javascript/code-along-tutor/section-20-react-router-01/src/pages/Products.js
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
-    return <h1>The Products Page</h1>;
+    return (
+        <section>
+            <h1>The Products Page</h1>
+            <ul>
+                <li>
+                    <Link to="/products/a-book">A Book</Link>
+                </li>
+                <li>
+                    <Link to="/products/a-carpet">A Carpet</Link>
+                </li>
+                <li>
+                    <Link to="/products/a-car">A Car</Link>
+                </li>
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
