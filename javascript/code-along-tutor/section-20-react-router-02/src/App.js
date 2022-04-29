9c9f53978ac5fa6c583bfd6e656a521f6fed77ff
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-04-29 13:59:11 +0700

    Section 21: Lazy loading

--- javascript/code-along-tutor/section-20-react-router-02/src/App.js
+++ javascript/code-along-tutor/section-20-react-router-02/src/App.js
@@ -1,30 +1,48 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import QuoteDetailPage from './pages/QuoteDetailPage';
-import QuoteListPage from './pages/QuoteListPage';
-import NewQuotePage from './pages/NewQuotePage';
 import Layout from './components/layout/Layout';
-import NotFoundPage from './pages/NotFoundPage';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+// import QuoteListPage from './pages/QuoteListPage';
+const QuoteListPage = React.lazy(() => import('./pages/QuoteListPage'));
+
+// import NewQuotePage from './pages/NewQuotePage';
+const NewQuotePage = React.lazy(() => import('./pages/NewQuotePage'));
+
+// import QuoteDetailPage from './pages/QuoteDetailPage';
+const QuoteDetailPage = React.lazy(() => import('./pages/QuoteDetailPage'));
+
+// import NotFoundPage from './pages/NotFoundPage';
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
 function App() {
     return (
         <Layout>
-            <Switch>
-                <Route path="/" exact>
-                    <Redirect to="/quotes" />
-                </Route>
-                <Route path="/quotes" exact>
-                    <QuoteListPage />
-                </Route>
-                <Route path="/quotes/:quoteId">
-                    <QuoteDetailPage />
-                </Route>
-                <Route path="/new-quote">
-                    <NewQuotePage />
-                </Route>
-                <Route path="*">
-                    <NotFoundPage />
-                </Route>
-            </Switch>
+            <Suspense
+                fallback={
+                    <div className="centered">
+                        <LoadingSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/quotes" />
+                    </Route>
+                    <Route path="/quotes" exact>
+                        <QuoteListPage />
+                    </Route>
+                    <Route path="/quotes/:quoteId">
+                        <QuoteDetailPage />
+                    </Route>
+                    <Route path="/new-quote">
+                        <NewQuotePage />
+                    </Route>
+                    <Route path="*">
+                        <NotFoundPage />
+                    </Route>
+                </Switch>
+            </Suspense>
         </Layout>
     );
 }
