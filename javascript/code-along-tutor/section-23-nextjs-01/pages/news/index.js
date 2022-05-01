7abe5655600bb6adda9abf109ea8b366dcd33d11
An Duong <antduong@cisco.com>
2022-05-01 11:17:06 +0700

    Section 23: NextJS-01 - Linking between pages

--- javascript/code-along-tutor/section-23-nextjs-01/pages/news/index.js
+++ javascript/code-along-tutor/section-23-nextjs-01/pages/news/index.js
@@ -1,5 +1,22 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 const NewsPage = () => {
-    return <h1>The News Page</h1>;
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        NextJS is a great framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/something-else">Something else</Link>
+                </li>
+            </ul>
+        </Fragment>
+    );
 };
 
 export default NewsPage;
