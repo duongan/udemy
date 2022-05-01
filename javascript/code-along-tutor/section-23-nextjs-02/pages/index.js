4baf475e754e82f6db426aa079b76cf7424d3871
An Duong <antduong@cisco.com>
2022-05-01 14:10:30 +0700

    Section 23: NextJS-02 - Layout Wrapper (_app.js file)

--- javascript/code-along-tutor/section-23-nextjs-02/pages/index.js
+++ javascript/code-along-tutor/section-23-nextjs-02/pages/index.js
@@ -1,24 +1,24 @@
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A first meetup',
-        image: 'https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg',
-        address: 'Some address 4, 12 some city',
-        description: 'This is a first meetup.',
-    },
-    {
-        id: 'm2',
-        title: 'A second meetup',
-        image: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg',
-        address: '123, some street, some city',
-        description: 'This is a second meetup.',
-    },
+  {
+    id: 'm1',
+    title: 'A first meetup',
+    image: 'https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg',
+    address: 'Some address 4, 12 some city',
+    description: 'This is a first meetup.',
+  },
+  {
+    id: 'm2',
+    title: 'A second meetup',
+    image: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg',
+    address: '123, some street, some city',
+    description: 'This is a second meetup.',
+  },
 ];
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
