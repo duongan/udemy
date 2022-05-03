527e683582592f0c09d8b31e40879da5ca415b20
An Duong <duongan1202@gmail.com>
2022-05-03 16:16:34 +0700

    Section 25: Project-01 - Replacing Redux by React Hooks

--- javascript/code-along-tutor/section-25-01/src/components/Products/ProductItem.js
+++ javascript/code-along-tutor/section-25-01/src/components/Products/ProductItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+// import { toggleFav } from '../../store/actions/products';
+// import { ProductsContext } from '../../context/products-context';
+import { useStore } from '../../hook-store/store';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
+const ProductItem = React.memo((props) => {
+  console.log('RENDERING');
+  // const dispatch = useDispatch();
+  // const toggleFav = useContext(ProductsContext).toggleFav;
+  const dispatch = useStore(false)[1];
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
+    // toggleFav(props.id);
+    dispatch('TOGGLE_FAV', props.id);
   };
 
   return (
@@ -26,6 +33,6 @@ const ProductItem = props => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
--- javascript/code-along-tutor/section-25-01/src/containers/Favorites.js
+++ javascript/code-along-tutor/section-25-01/src/containers/Favorites.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
+// import { ProductsContext } from '../context/products-context';
+import { useStore } from '../hook-store/store';
 import './Products.css';
 
-const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+const Favorites = (props) => {
+  // const favoriteProducts = useSelector(state =>
+  //   state.shop.products.filter(p => p.isFavorite)
+  // );
+  // const favoriteProducts = useContext(ProductsContext).products.filter(
+  //   (p) => p.isFavorite
+  //   );
+  const state = useStore()[0];
+  const favoriteProducts = state.products.filter((p) => p.isFavorite);
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {favoriteProducts.map((prod) => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
