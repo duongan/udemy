e7d5ed1e1adb13ecff294c4291277deb1f6d3f9a
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-05-06 10:22:16 +0700

    Section 28: P01 - Understanding useReducer

--- javascript/code-along-tutor/section-28-01/src/components/Ingredients/Ingredients.js
+++ javascript/code-along-tutor/section-28-01/src/components/Ingredients/Ingredients.js
@@ -1,12 +1,26 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there!');
+  }
+};
+
 function Ingredients() {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [ingredients, setIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -31,26 +45,31 @@ function Ingredients() {
         return response.json();
       })
       .then((responseData) => {
-        setIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
+        // setIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient },
+        // ]);
+        dispatch({
+          type: 'ADD',
+          ingredient: { id: responseData.name, ...ingredient },
+        });
       });
   };
 
   const removeIngredientHandler = (id) => {
     setIsLoading(true);
     fetch(
-      `https://react-http-3b4e8-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients/${id}.jsn`,
+      `https://react-http-3b4e8-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients/${id}.json`,
       {
         method: 'DELETE',
       }
     )
       .then((response) => {
         setIsLoading(false);
-        setIngredients((prevIngredients) =>
-          prevIngredients.filter((ig) => ig.id !== id)
-        );
+        // setIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ig) => ig.id !== id)
+        // );
+        dispatch({ type: 'DELETE', id });
       })
       .catch((error) => {
         setError('Something went wrong!');
@@ -59,7 +78,8 @@ function Ingredients() {
   };
 
   const filterIngredientsHandler = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    // setIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
   const clearError = () => {
