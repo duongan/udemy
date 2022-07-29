6b0c3727c9f65ff05a62a34eebb40659af4afaf6
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-07-29 07:07:08 +0700

    [MyTodo] Adding User Profile popover

--- reactjs/playing-around/my-todo/src/components/Header/index.js
+++ reactjs/playing-around/my-todo/src/components/Header/index.js
@@ -1,5 +1,8 @@
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
 import { logOut } from '../../store/thunkActionCreators';
 import styles from './Header.module.scss';
 
@@ -7,12 +10,40 @@ const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userInfo = useSelector((state) => state.user.userInfo);
+  const [showProfilePopover, setShowProfilePopover] = useState(false);
+  const popoverRef = useRef();
+  const userIconRef = useRef();
+
   const clickLogoHandler = () => {
     navigate('/');
   };
-  const logout = () => {
+
+  const clickUserIconHandler = () => {
+    console.log('tai day 111');
+    setShowProfilePopover(!showProfilePopover);
+  };
+
+  const logout = (e) => {
+    e.preventDefault();
     dispatch(logOut());
   };
+
+  useEffect(() => {
+    const showAndHidePopoverListener = (e) => {
+      if (
+        !showProfilePopover ||
+        userIconRef.current.contains(e.target) ||
+        popoverRef.current.contains(e.target)
+      ) {
+        return;
+      }
+      setShowProfilePopover(false);
+    };
+    document.addEventListener('click', showAndHidePopoverListener);
+    return () => {
+      document.removeEventListener('click', showAndHidePopoverListener);
+    };
+  }, [showProfilePopover]);
 
   return (
     <div className={styles.container}>
@@ -20,9 +51,25 @@ const Header = () => {
         MyTodo
       </div>
       <div></div>
-      <div className={styles.uname} onClick={logout}>
-        {userInfo ? userInfo.email : ''}
+      <div
+        className={styles.profileIcon}
+        onClick={clickUserIconHandler}
+        ref={userIconRef}
+      >
+        {/* {userInfo ? userInfo.email : ''} */}
+        <FontAwesomeIcon icon={faCircleUser} />
       </div>
+      {showProfilePopover && (
+        <div className={styles.profilePopover} ref={popoverRef}>
+          <div>{userInfo ? userInfo.email : ''}</div>
+          <a href="#" onClick={() => {}}>
+            Profile
+          </a>
+          <a href="#" onClick={logout}>
+            Sign out
+          </a>
+        </div>
+      )}
     </div>
   );
 };
