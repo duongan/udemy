abfb19b62b9dc2e7fea6f368b64ce7b5d8d251a8
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-08-26 12:46:41 +0700

    [NodeJS-Course] Section 6: Working with EJS Template Engine

--- nodejs/course-source-code/section-5-working-with-express/app.js
+++ nodejs/course-source-code/section-5-working-with-express/app.js
@@ -5,22 +5,21 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-
-// console.log('expressHbs', expressHbs);
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs.engine({
-    extname: 'hbs',
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-  })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs.engine({
+//     extname: 'hbs',
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//   })
+// );
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
