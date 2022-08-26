2a2047ea42de4d3cf4cfbf28bb34e4faa940957a
An Duong <duongan1202@gmail.com>
2022-08-26 10:26:26 +0700

    [NodeJS-Course] Section 6: Templating Engines - An Overview, Installing and Implementing Pug

--- nodejs/course-source-code/section-5-working-with-express/app.js
+++ nodejs/course-source-code/section-5-working-with-express/app.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
