14d24de1aa1e9efddaa912091b8a15e635fe558b
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-08-29 10:37:17 +0700

    [NodeJS-Course] Section 11: Adding a One-to-Many relationship

--- nodejs/course-source-code/section-5-working-with-express/app.js
+++ nodejs/course-source-code/section-5-working-with-express/app.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -33,8 +35,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
     // console.log(result);
     app.listen(3000);
