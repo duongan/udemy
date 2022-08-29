40a62029828875a4e79b415ff3f877b4e8c19a79
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-08-29 14:11:41 +0700

    [NodeJS-Course] Section 11: Adding existing products and retrieving cart items

--- nodejs/course-source-code/section-5-working-with-express/controllers/shop.js
+++ nodejs/course-source-code/section-5-working-with-express/controllers/shop.js
@@ -156,6 +156,7 @@ exports.postCart = (req, res, next) => {
   // res.redirect('/cart');
 
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -167,16 +168,17 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
       if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => console.log(err));
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect('/cart');
