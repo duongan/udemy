3dd030c89974f5a93cb65d40d6ffe26ca14c77dc
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-08-31 15:55:11 +0700

    [NodeJS-Course] Section 18: Adding validation to Login

--- nodejs/course-source-code/section-15-authentication/controllers/auth.js
+++ nodejs/course-source-code/section-15-authentication/controllers/auth.js
@@ -25,6 +25,11 @@ exports.getLogin = (req, res, next) => {
     path: '/login',
     pageTitle: 'Login',
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+    },
+    validationErrors: [],
   });
 };
 
@@ -57,14 +62,29 @@ exports.postLogin = (req, res, next) => {
       path: '/login',
       pageTitle: 'Login',
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email,
+        password,
+      },
+      validationErrors: errors.array(),
     });
   }
 
   User.findOne({ email })
     .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          oldInput: {
+            email,
+            password,
+          },
+          validationErrors: [],
+        });
+        // req.flash('error', 'Invalid email or password.');
+        // return res.redirect('/login');
       }
 
       bcrypt
@@ -78,8 +98,18 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
-          req.flash('error', 'Invalid email or password.');
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password.',
+            oldInput: {
+              email,
+              password,
+            },
+            validationErrors: [],
+          });
+          // req.flash('error', 'Invalid email or password.');
+          // res.redirect('/login');
         })
         .catch((err) => {
           console.log(err);
