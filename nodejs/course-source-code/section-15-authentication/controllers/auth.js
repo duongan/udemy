9839beae7da5856c3f9b38ad45398543a5933aed
An Duong <duongan1202@gmail.com>
2022-08-31 11:20:48 +0700

    [NodeJS-Course] Section 17: Adding logic to update the password

--- nodejs/course-source-code/section-15-authentication/controllers/auth.js
+++ nodejs/course-source-code/section-15-authentication/controllers/auth.js
@@ -171,12 +171,44 @@ exports.getNewPassword = (req, res, next) => {
       } else {
         message = null;
       }
+      if (!user) {
+        req.flash('error', 'Invalid reset password request.');
+        return res.redirect('/reset');
+      }
       res.render('auth/new-password', {
         path: '/new-password',
         pageTitle: 'New Password',
         errorMessage: message,
         userId: user._id.toString(),
+        passwordToken: token,
       });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = null;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
     })
     .catch((err) => console.log(err));
 };
