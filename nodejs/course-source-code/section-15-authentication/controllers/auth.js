c7a129479d9817154c6d8a243c2cdd1d9cce22c3
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-08-31 14:43:40 +0700

    [NodeJS-Course] Section 18: Setup and using Express Validation

--- nodejs/course-source-code/section-15-authentication/controllers/auth.js
+++ nodejs/course-source-code/section-15-authentication/controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 // const nodemailer = require('nodemailer');
 // const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -74,6 +75,14 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email })
     .then((userDoc) => {
       if (userDoc) {
