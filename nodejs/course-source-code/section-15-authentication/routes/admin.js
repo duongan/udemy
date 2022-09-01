2195dda805ff6a3322273218381e12fccf216f02
An Duong <duongan1202@gmail.com>
2022-09-01 21:34:38 +0700

    [NodeJS-Course] Section 19: 320. Handling multipart form data

--- nodejs/course-source-code/section-15-authentication/routes/admin.js
+++ nodejs/course-source-code/section-15-authentication/routes/admin.js
@@ -21,7 +21,7 @@ router.post(
     body('title').isString().isLength({ min: 3 }).trim(),
     body('price').isFloat(),
     body('description').isLength({ min: 5, max: 400 }).trim(),
-    body('imageUrl').isURL(),
+    // body('imageUrl').isURL(),
   ],
   isAuth,
   adminController.postAddProduct
