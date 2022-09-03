1e0810b7949b2e8a98839903b4b4da4e683e845b
An Duong <duongan1202@gmail.com>
2022-09-03 09:33:43 +0700

    [NodeJS-Course] Section 19: 342-343. Adding dynamic pagination buttons and reusing the pagination logic

--- nodejs/course-source-code/section-15-authentication/controllers/shop.js
+++ nodejs/course-source-code/section-15-authentication/controllers/shop.js
@@ -6,17 +6,41 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numberProducts) => {
+      totalItems = numberProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
+    // Product.find()
+    //   .then((products) => {
+    //     res.render('shop/product-list', {
+    //       prods: products,
+    //       pageTitle: 'All Products',
+    //       path: '/products',
+    //     });
+    //   })
     .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
@@ -42,7 +66,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const { page } = req.query;
+  const page = +req.query.page || 1;
   let totalItems;
 
   Product.find()
@@ -58,7 +82,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
