0574af676452afc69203300b8f66d4dd33c380c9
An Duong <duongan1202@gmail.com>
2022-09-03 20:34:43 +0700

    [NodeJS-Course] Section 25: 377. Storing posts in the database

--- nodejs/course-source-code/section-24-rest-api/controllers/feed.js
+++ nodejs/course-source-code/section-24-rest-api/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -27,17 +29,22 @@ exports.createPost = (req, res, next) => {
   }
 
   const { title, content } = req.body;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: {
-        name: 'AnD',
-      },
-      createdAt: new Date(),
+  const post = new Post({
+    title,
+    content,
+    imageUrl: 'images/a-duck.jpg',
+    creator: {
+      name: 'An Duong',
     },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result,
+      });
+    })
+    .catch((err) => console.log(err));
 };
