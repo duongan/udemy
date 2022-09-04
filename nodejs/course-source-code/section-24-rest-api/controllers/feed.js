49a487c594e0f9e0c732fa449663c4dae9da1739
An Duong <duongan1202@gmail.com>
2022-09-04 14:21:47 +0700

    [NodeJS-Course] Section 25: 393. Connecting posts and users

--- nodejs/course-source-code/section-24-rest-api/controllers/feed.js
+++ nodejs/course-source-code/section-24-rest-api/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const { page: currentPage = 1 } = req.query;
@@ -48,21 +49,28 @@ exports.createPost = (req, res, next) => {
 
   const imageUrl = req.file.path.replace('\\', '/');
   const { title, content } = req.body;
+  let creator;
   const post = new Post({
     title,
     content,
     imageUrl,
-    creator: {
-      name: 'An Duong',
-    },
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: 'Post created successfully!',
-        post: result,
+        post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
