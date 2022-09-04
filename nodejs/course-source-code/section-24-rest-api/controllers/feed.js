79f302dccca482ea1656419378fe7a0818cd5d64
An Duong <duongan1202@gmail.com>
2022-09-04 14:38:33 +0700

    [NodeJS-Course] Section 25: 395. Clearing Post-User relations

--- nodejs/course-source-code/section-24-rest-api/controllers/feed.js
+++ nodejs/course-source-code/section-24-rest-api/controllers/feed.js
@@ -174,7 +174,14 @@ exports.deletePost = (req, res, next) => {
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     })
-    .then((result) => {
+    .then(() => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(() => {
       res.status(200).json({
         message: 'Deleted post.',
       });
