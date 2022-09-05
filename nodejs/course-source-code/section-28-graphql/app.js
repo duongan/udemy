c905fbcb4fbebc8876d43fc1164e6d58b0c107c2
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-09-05 10:41:32 +0700

    [NodeJS-Course] Section 28: Init source code

---
+++ nodejs/course-source-code/section-28-graphql/app.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const httpServer = createServer(app);
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${uuidv4()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded (<form>)
+app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const { statusCode, message, data } = error || 500;
+  res.status(statusCode).json({ message, data });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://andt_learning:F5zDpHO6ZROiSZUT@cluster0.l89rk.mongodb.net/messages?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    httpServer.listen(8080);
+  })
+  .catch((err) => console.log(err));
