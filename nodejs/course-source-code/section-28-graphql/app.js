210bff8a85392c9809eb3147e3e372b41145f559
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-09-05 10:54:56 +0700

    [NodeJS-Course] Section 28: 421. Understanding the setup and writing the first query

--- nodejs/course-source-code/section-28-graphql/app.js
+++ nodejs/course-source-code/section-28-graphql/app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const { graphqlHTTP } = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
 const httpServer = createServer(app);
@@ -45,6 +49,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
