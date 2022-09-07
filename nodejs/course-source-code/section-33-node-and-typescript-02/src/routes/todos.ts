d35be149918f633d44f1888279f2a0fec0b7176a
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2022-09-07 15:18:27 +0700

    [NodeJS-Course] Section 33: Working with Express and TypeScript

---
+++ nodejs/course-source-code/section-33-node-and-typescript-02/src/routes/todos.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+
+import { Todo } from '../models/todo';
+
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
+let todos: Array<Todo> = []; // or const todos: Todo[] = [];
+
+const router = Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({ todos });
+});
+
+router.post('/todo', (req, res, next) => {
+  const body = req.body as RequestBody;
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: body.text,
+  };
+
+  todos.push(newTodo);
+
+  res.status(201).json({ message: 'Added todo', todos, todo: newTodo });
+});
+
+router.put('/todo/:todoId', (req, res, next) => {
+  const body = req.body as RequestBody;
+  const params = req.params as RequestParams;
+  const tid = params.todoId;
+  const todoIndex = todos.findIndex((todo) => todo.id === tid);
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
+    return res.status(200).json({ message: 'Updated todo', todos });
+  }
+  res.status(404).json({ message: 'Could not find todo for this id.' });
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+  const params = req.params as RequestParams;
+  todos = todos.filter((todo) => todo.id !== params.todoId);
+  res.status(200).json({ message: 'Deleted todo', todos });
+});
+
+export default router;
