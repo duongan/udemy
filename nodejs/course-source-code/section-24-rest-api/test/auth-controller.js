bdd03526cd32f5dc6e226481afe262c13b40d98b
An Duong <duongan1202@gmail.com>
2022-09-07 08:17:08 +0700

    [NodeJS-Course] Section 30: 473. Setting up a testing database

--- nodejs/course-source-code/section-24-rest-api/test/auth-controller.js
+++ nodejs/course-source-code/section-24-rest-api/test/auth-controller.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 
@@ -25,5 +26,23 @@ describe('Auth Controller - Login', function () {
     });
 
     User.findOne.restore();
+  });
+
+  it('should send a response with a valid user status for an existing user', function (done) {
+    mongoose
+      .connect(
+        'mongodb+srv://andt_learning:F5zDpHO6ZROiSZUT@cluster0.l89rk.mongodb.net/test-messages?retryWrites=true&w=majority'
+      )
+      .then(() => {
+        const user = new User({
+          email: 'test@test.com',
+          password: '123456',
+          name: 'Test',
+          posts: [],
+        });
+        return user.save();
+      })
+      .then(() => {})
+      .catch((err) => console.log(err));
   });
 });
