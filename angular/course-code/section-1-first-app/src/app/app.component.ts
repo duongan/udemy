a2d56e60000f9ea713c6c6f1d2d9df326a9c149e
An Duong <duongan1202@gmail.com>
2022-09-09 09:46:59 +0700

    [Angular] Section 1: 13. A basic project setup using Bootstrap for Styling

--- angular/course-code/section-1-first-app/src/app/app.component.ts
+++ angular/course-code/section-1-first-app/src/app/app.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'An Duong';
-}
+export class AppComponent {}
