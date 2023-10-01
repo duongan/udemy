7b1b62c5b256625344a086b2c58bb2a4b66061dc
DESKTOP-NTU0LN1\Admin <duongan1202@gmail.com>
2023-10-01 19:39:13 +0700

    Fundamentals of web components: 4.20 Styling - Part & CSS Custom

--- fundamentals-of-web-components/section-2-custom-elements/src/card.js
+++ fundamentals-of-web-components/section-2-custom-elements/src/card.js
@@ -3,10 +3,9 @@ cardTemplate.innerHTML = /* html */`
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <div class="card">
     <div class="card-header">
-      Card header will be here
       <slot name="card-header">Card Header</slot>
     </div>
-    <div class="card-body">
+    <div part="body-of-card" class="card-body">
       <slot name="card-body">Card Body</slot>
     </div>
   </div>
